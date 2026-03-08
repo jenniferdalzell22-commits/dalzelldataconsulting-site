873b7f0ae27ef93bf524a88baf3378c96e2f3391
jenniferdalzell22-commits <263718733+jenniferdalzell22-commits@users.noreply.github.com>
2026-03-08 09:25:29 -0700

    Add files via upload

---
+++ inc/sendemail.php
@@ -0,0 +1,6 @@
+$mail->	Host = 'techskillsmastery.com';
+$mail->	SMTPAuth = true;
+$mail->	Username = '_mainaccount@techskillsmastery.com';
+$mail->	Password = 'cur120823';
+$mail->	SMTPSecure = true;
+$mail->	Port = 993;
