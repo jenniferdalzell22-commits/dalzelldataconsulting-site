336fb34f73b08ad1a812e23b6fd5ed5565b2a182
jenniferdalzell22-commits <263718733+jenniferdalzell22-commits@users.noreply.github.com>
2026-03-19 14:48:54 -0400

    updates mar19

---
+++ temp/sendemail.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+	<script type="text/javascript">
+			</script>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title><?php echo htmlspecialchars((isset($seoTitle) && $seoTitle !== "") ? $seoTitle : "Contacts"); ?></title>
+	<base href="{{base_url}}" />
+	<?php echo isset($sitemapUrls) ? generateCanonicalUrl($sitemapUrls) : ""; ?>	
+	
+						<meta name="viewport" content="width=device-width, initial-scale=1" />
+					<meta name="description" content="<?php echo htmlspecialchars((isset($seoDescription) && $seoDescription !== "") ? $seoDescription : "Contacts"); ?>" />
+			<meta name="keywords" content="<?php echo htmlspecialchars((isset($seoKeywords) && $seoKeywords !== "") ? $seoKeywords : "Contacts"); ?>" />
+		
+	<!-- Facebook Open Graph -->
+		<meta property="og:title" content="<?php echo htmlspecialchars((isset($seoTitle) && $seoTitle !== "") ? $seoTitle : "Contacts"); ?>" />
+			<meta property="og:description" content="<?php echo htmlspecialchars((isset($seoDescription) && $seoDescription !== "") ? $seoDescription : "Contacts"); ?>" />
+			<meta property="og:image" content="<?php echo htmlspecialchars((isset($seoImage) && $seoImage !== "") ? "{{base_url}}".$seoImage : ""); ?>" />
+			<meta property="og:type" content="article" />
+			<meta property="og:url" content="{{curr_url}}" />
+		<!-- Facebook Open Graph end -->
+
+			<script src="js/common-bundle.js?ts=20240129220220" type="text/javascript"></script>
+	<script src="js/a188dda102a402fbc509d1055efb2508-bundle.js?ts=20240129220220" type="text/javascript"></script>
+	<link href="css/common-bundle.css?ts=20240129220220" rel="stylesheet" type="text/css" />
+	<link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i&amp;subset=cyrillic,cyrillic-ext,greek,greek-ext,latin,latin-ext,vietnamese" rel="stylesheet" type="text/css" />
+	<link href="https://fonts.googleapis.com/css?family=Poppins:400&amp;subset=latin" rel="stylesheet" type="text/css" />
+	<link href="css/a188dda102a402fbc509d1055efb2508-bundle.css?ts=20240129220220" rel="stylesheet" type="text/css" id="wb-page-stylesheet" />
+	<ga-code/>
+	<script type="text/javascript">
+	window.useTrailingSlashes = true;
+	window.disableRightClick = false;
+	window.currLang = 'en';
+</script>
+		
+	<!--[if lt IE 9]>
+	<script src="js/html5shiv.min.js"></script>
+	<![endif]-->
+
+	</head>
+
+
+<body class="site site-lang-en<?php if (isset($wbPopupMode) && $wbPopupMode) echo ' popup-mode'; ?> " <?php ?>><div id="wb_root" class="root wb-layout-vertical"><div class="wb_sbg"></div><div id="wb_header_a188dda102a402fbc509d1055efb2508" class="wb_element wb-layout-element" data-plugin="LayoutElement"><div class="wb_content wb-layout-vertical"><div id="a188dda0e1de70feb9e7024a7cc03c01" class="wb_element wb-anim-entry wb-anim wb-anim-fade-in-none wb-layout-element" data-plugin="LayoutElement" data-wb-anim-entry-time="0.6" data-wb-anim-entry-delay="0"><div class="wb_content wb-layout-horizontal"><div id="a188dda0e1de717303b5959e7ccfe1cb" class="wb_element wb-layout-element" data-plugin="LayoutElement"><div class="wb_content wb-layout-horizontal"><div id="a188dda0e1de72b58a936d0a26d30608" class="wb_element wb_element_picture" data-plugin="Picture" title=""><div class="wb_picture_wrap" style="height: 100%"><div class="wb-picture-wrapper" style="overflow: visible; display: flex"><a href="{{base_url}}"><svg xmlns="http://www.w3.org/2000/svg" width="1793.982" height="1793.982" viewBox="0 0 1793.982 1793.982" style="direction: ltr; color:#ffffff"><text x="65.501415" y="1537.02" font-size="1792" fill="currentColor" style='font-family: "FontAwesome"'></text></svg></a></div></div></div><div id="a188dda0e1de7351ed0234958c5dcfa5" class="wb_element wb_text_element" data-plugin="TextArea" style=" line-height: normal;"><h3 class="wb-stl-heading3" style="text-align: center;"><span class="wb-stl-highlight">TechSkillsMastery.com</span></h3>
+</div></div></div><div id="a18b5bfb42a300286efcc55cbcc9a95c" class="wb_element wb-layout-element" data-plugin="LayoutElement"><div class="wb_content wb-layout-vertical"><div id="a188dda0e1de748108454f91fa30604b" class="wb_element wb-menu wb-prevent-layout-click wb-menu-mobile" data-plugin="Menu"><a class="btn btn-default btn-collapser"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></a><ul class="hmenu" dir="ltr"><li class=""><a href="https://techskillsmastery.com/index.htm">Home</a></li><li class=""><a href="https://techskillsmastery.com/service.htm">Services</a></li><li class=""><a href="https://techskillsmastery.com/portfolio.htm">Portfolio</a></li><li class=""><a href="https://techskillsmastery.com/about.htm">About</a></li><li class="wb_this_page_menu_item"><a href="Contacts/">Contact</a></li></ul><div class="clearfix"></div></div></div></div></div></div></div></div><div id="wb_main_a188dda102a402fbc509d1055efb2508" class="wb_element wb-layout-element" data-plugin="LayoutElement"><div class="wb_content wb-layout-vertical"><div id="a188dda0e1de766ec7eee9610cef24ee" class="wb_element wb-layout-element" data-plugin="LayoutElement"><div class="wb_content wb-layout-horizontal"><div id="a188dda0e1de773209e0186a02a62ce9" class="wb_element wb-anim-entry wb-anim wb-anim-fade-in-none wb-layout-element" data-plugin="LayoutElement" data-wb-anim-entry-time="0.6" data-wb-anim-entry-delay="0"><div class="wb_content wb-layout-vertical"><div id="a188dda0e1de7845e7b15abe542b57dd" class="wb_element wb_text_element" data-plugin="TextArea" style=" line-height: normal;"><h1 class="wb-stl-heading1" style="text-align: center;"><font size="45"><strong><span style="color:rgba(255,255,255,1);">Turning Data into Insights,  <br><br>Automation into Success</span></font></strong></h1>
+</div></div></div></div></div><div id="a188dda0e1de796eb8f98c89a386697e" class="wb_element wb-layout-element" data-plugin="LayoutElement"><div class="wb_content wb-layout-vertical"><div id="a188dda0e1de7a7bbc7d34d2bc20c5e3" class="wb_element wb_text_element" data-plugin="TextArea" style=" line-height: normal;"><h2 class="wb-stl-heading3" style="text-align: center;"><strong><span style="color:#000000;">Get Started: <br><br> Ready to transform your financial reporting? <br><br> Contact me today to discuss your project, <br> and let’s make your business more efficient!</span></strong></h2>
+</div><div id="a188dda0e1de7bc0de7027212346178f" class="wb_element wb-layout-element" data-plugin="LayoutElement"><div class="wb_content wb-layout-horizontal"><div id="a188dda0e1de7cc84800e041abf1af90" class="wb_element wb_text_element" data-plugin="TextArea" style=" line-height: normal;"><p class="wb-stl-normal"><font size="6">NewYork, USA</font><br><br><images align="Left" src="/images/westchester_cty.jfif" alt="Westchester County" height="95%" width="95%"></p>
+</div><div id="a188dda0e1de7d2287bc66b5e62985a0" class="wb_element" data-plugin="Form"><form id="a188dda0e1de7d2287bc66b5e62985a0_form" class="wb_form wb_mob_form wb_form_ltr wb_form_vertical" method="post" enctype="multipart/form-data" action="__wb_curr_url__"><input type="hidden" name="wb_form_id" value="aa1bdb29"><input type="hidden" name="wb_form_uuid" value="4753950d"><textarea name="message" rows="3" cols="20" class="hpc" autocomplete="off"></textarea><table><tr><th>Name<span class="text-danger">&nbsp;*</span></th><td><input type="hidden" name="wb_input_0" value="Name"><div><input class="form-control form-field" type="text" value="" placeholder="" maxlength="255" name="wb_input_0" required="required"></div></td></tr><tr><th>E-mail<span class="text-danger">&nbsp;*</span></th><td><input type="hidden" name="wb_input_1" value="E-mail"><div><input class="form-control form-field" type="text" value="" placeholder="" maxlength="255" name="wb_input_1" required="required"></div></td></tr><tr class="area-row"><th>Message<span class="text-danger">&nbsp;*</span></th><td><input type="hidden" name="wb_input_2" value="Message"><div><textarea class="form-control form-field form-area-field" rows="4" placeholder="" name="wb_input_2" required="include"></textarea></div></td></tr><tr class="form-footer"><td colspan="2" class="text-right"><button type="submit" class="btn btn-default"><span>Send Message</span></button></td></tr></table><?php if (isset($wbPopupMode) && $wbPopupMode): ?><input type="hidden" name="wb_popup_mode" value="1" /><?php endif; ?></form><script type="text/javascript">
+			<?php $wb_form_id = sessionOrGlobalVar("wb_form_id"); if ($wb_form_id == "aa1bdb29") { ?>
+				<?php popSessionOrGlobalVar("wb_form_id"); ?>
+				<?php $wb_form_send_success = popSessionOrGlobalVar("wb_form_send_success"); ?>
+				var formValues = <?php echo json_encode(popSessionOrGlobalVar("post")); ?>;
+				var formErrors = <?php echo json_encode(popSessionOrGlobalVar("formErrors")); ?>;
+				wb_form_validateForm("aa1bdb29", formValues, formErrors);
+				<?php if (($wb_form_send_state = popSessionOrGlobalVar("wb_form_send_state"))) { ?>
+					<?php if (($wb_form_popup_mode = popSessionOrGlobalVar("wb_form_popup_mode"))) { ?>
+					if (window !== window.parent && window.parent.postMessage) {
+						var data = {
+							event: "wb_contact_form_sent",
+							data: {
+								state: "<?php echo str_replace('"', '\"', $wb_form_send_state); ?>",
+								type: "<?php echo $wb_form_send_success ? "success" : "danger"; ?>"
+							}
+						};
+						window.parent.postMessage(data, "<?php echo str_replace('"', '\"', popSessionOrGlobalVar("wb_target_origin")); ?>");
+					}
+					<?php } else { ?>
+					wb_show_alert("<?php echo str_replace(array('"', "\r", "\n"), array('\"', "", "<br/>"), $wb_form_send_state); ?>", "<?php echo $wb_form_send_success ? "success" : "danger"; ?>");
+					<?php } ?>
+					<?php $wb_form_send_success = false; $wb_form_send_state = null; $wb_form_popup_mode = false; ?>
+				<?php } ?>
+			<?php } ?>
+			</script></div></div></div></div></div><div id="a188dda0e1de7e9e0bfa27b780c77178" class="wb_element wb-layout-element" data-plugin="LayoutElement"><div class="wb_content wb-layout-vertical"></div></div></div></div><div id="wb_footer_a188dda102a402fbc509d1055efb2508" class="wb_element wb-layout-element" data-plugin="LayoutElement"><div class="wb_content wb-layout-vertical"><div id="wb_footer_c" class="wb_element" data-plugin="WB_Footer" style="text-align: center; width: 100%;"><div class="wb_footer"></div><script type="text/javascript">
+			$(function() {
+				var footer = $(".wb_footer");
+				var html = (footer.html() + "").replace(/^\s+|\s+$/g, "");
+				if (!html) {
+					footer.parent().remove();
+					footer = $("#footer, #footer .wb_cont_inner");
+					footer.css({height: ""});
+				}
+			});
+			</script></div></div></div></div>{{hr_out}}</body>
+</html>
